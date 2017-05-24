ed9e72e45f46f665829e9b177415db385fec1481
pyfl-io <pyflblog@gmail.com>
2017-05-25 02:25:19 +0530

    pagination of blog posts

--- config.py
+++ config.py
@@ -9,11 +9,12 @@ class Config:
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = 'mr.shubhshar@gmail.com'
-    MAIL_PASSWORD = 'pythonflaskjavascript'
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'mr.shubhshar@gmail.com'
-    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN') or 'Flasky_Admin'
+    FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
+    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+    FLASKY_POSTS_PER_PAGE = 20
 
     @staticmethod
     def init_app(app):
